ff5c10437e7e443fde0b9944e9c032b7d645f275
Oleginator1 <152632140+Oleginator1@users.noreply.github.com>
2025-06-25 17:40:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@ using System.Globalization;
 
 namespace Exemplu
 {
+   public class Book
+   {
+       public int Id { get; set; }
+       public string Title { get; set; }
+       public string Author { get; set; }
+       public int Year { get; set; }
+       public int CopiesAvailable { get; set; }
+   }
    
     public class MyClass
     {
