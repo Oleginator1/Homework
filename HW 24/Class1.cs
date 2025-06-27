ab6e1b63c7bade5a1e3a40107490ec59e4806f89
Oleginator1 <152632140+Oleginator1@users.noreply.github.com>
2025-06-27 17:20:36 +0300

    Add files via upload

---
+++ HW 24/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }    
+    }
+
+    public interface IPayment
+    {
+        public void ProcessPayment(Order order);
+    }
+
+    public class CreditCardPayment : IPayment
+    {
+        public void ProcessPayment(Order order)
+        {
+            Console.WriteLine("Processing credit card payment...");
+        }
+    }
+
+    public class PayPalPayment : IPayment
+    {
+        public void ProcessPayment(Order order)
+        {
+            Console.WriteLine("Processing PayPal payment...");
+        }
+    }
+    
+    public class BankTrasferPayment : IPayment
+    {
+        public void ProcessPayment(Order order)
+        {
+            Console.WriteLine("Processing bank transfer payment...");
+        }
+    }
+
+    public class OrderRepository()
+    {
+        public void SaveToDatabase(Order order)
+        {
+            Console.WriteLine("Saving order to database...");
+        }
+
+
+        public void LoadFromDatabase(int orderId)
+        {
+            Console.WriteLine("Loading order from database...");
+        }
+    }
+
+}
---
+++ HW 24/Program.cs
@@ -0,0 +1,31 @@
+﻿using Exercise;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exemplu
+{
+   
+    public class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1,
+                Amount = 100
+            };
+
+            IPayment payment = new CreditCardPayment();
+            payment.ProcessPayment(order);
+            
+
+            OrderRepository orderRepository = new OrderRepository();
+            orderRepository.SaveToDatabase(order);
+            orderRepository.LoadFromDatabase(1);
+
+        }
+      
+
+    }
+}
