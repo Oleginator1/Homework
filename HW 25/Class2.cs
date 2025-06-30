cff1c77ee289c52a32b18054323dcfad59cdb650
Oleginator1 <152632140+Oleginator1@users.noreply.github.com>
2025-06-30 19:42:13 +0300

    Add files via upload

---
+++ HW 25/Class2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public interface IWorker
+{
+    void Work();    
+}
+
+public interface IEat
+{
+    void Eat();
+}
+
+
+public class HumanWorker : IWorker, IEat
+{
+    public void Work()
+    {
+        Console.WriteLine("Human working...");
+    }
+
+
+    public void Eat()
+    {
+        Console.WriteLine("Human eating...");
+    }
+}
+
+
+public class RobotWorker : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Robot working...");
+    }
+}
+
+
+public class Worker
+{
+    private readonly IWorker worker;
+    
+    public Worker(IWorker worker)
+    {
+        this.worker = worker; 
+    }
+    
+    public void Working()
+    {
+        worker.Work();
+    }
+}
+
+public class Eater
+{
+    private readonly IEat worker;
+
+    public Eater(IEat worker)
+    {
+        this.worker = worker;
+    }
+
+    public void Eating()
+    {
+        worker.Eat();
+    }
+}
---
+++ HW 25/Program.cs
@@ -0,0 +1,29 @@
+﻿using Exercise;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exemplu
+{
+   
+    public class MyClass
+    {
+        static void Main(string[] args)
+        {
+           IWorker hworker = new HumanWorker();
+           IWorker rworker = new RobotWorker();
+           IEat eater = new HumanWorker();
+            
+           Worker humanWorker = new Worker(hworker);
+           Worker robotWorker = new Worker(rworker);
+           Eater eater1 = new Eater(eater);
+
+            humanWorker.Working();
+            robotWorker.Working();
+            eater1.Eating();
+
+        }
+      
+
+    }
+}
