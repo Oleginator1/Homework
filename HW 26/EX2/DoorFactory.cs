d11be42811af1684dd84f18a6aeced0751442436
Oleginator1 <152632140+Oleginator1@users.noreply.github.com>
2025-07-02 18:24:55 +0300

    Add files via upload

---
+++ HW 26/EX2/DoorFactory.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public interface IDoor
+    {
+        string Material {  get; }
+        string Size { get; }
+        string Color { get; }
+        void Display();
+    }
+
+    public class WoodenDoor : IDoor
+    {
+        public string Material => "Wood";
+        public string Size { get; set; }
+        public string Color { get; set; }
+
+        public WoodenDoor(string size, string color)
+        {
+            Size = size;
+            Color = color;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Material: {Material}");
+            Console.WriteLine($"Size: {Size}");
+            Console.WriteLine($"Color: {Color}");
+        }
+    }
+
+    public class MetalDoor : IDoor
+    {
+        public string Material => "Metal";
+        public string Size { get; set; }
+        public string Color { get; set; }
+
+        public MetalDoor(string size, string color)
+        {
+            Size = size;
+            Color = color;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Material: {Material}");
+            Console.WriteLine($"Size: {Size}");
+            Console.WriteLine($"Color: {Color}");
+        }
+    }
+
+
+    public interface IDoorFacotory
+    {
+        IDoor CreateDoor(string size, string color);
+    }
+
+    public class WoodenDoorFactory : IDoorFacotory
+    {
+        public IDoor CreateDoor(string size, string color)
+        {
+            return new WoodenDoor(size, color);
+        }
+    }
+
+    public class MetalDoorFactory : IDoorFacotory
+    {
+        public IDoor CreateDoor(string size, string color)
+        {
+            return new MetalDoor(size, color);
+        }
+    }
+
+    public class FactoryManager
+    {
+        private readonly IDoorFacotory doorFacotry;
+        public FactoryManager(IDoorFacotory doorFacotry)
+        {
+            this.doorFacotry = doorFacotry;
+        }
+
+        public void ShowDoor(string size, string color)
+        {
+            var door = doorFacotry.CreateDoor(size, color);
+            door.Display();
+        }
+
+    }
+
+}
---
+++ HW 26/EX2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Exercise;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exemplu
+{
+   
+    public class MyClass
+    {
+        static void Main(string[] args)
+        {
+            FactoryManager woodManager = new FactoryManager(new WoodenDoorFactory());
+            Console.WriteLine("Wooden Door");
+            woodManager.ShowDoor("180x70", "Oak");
+
+            Console.WriteLine();
+
+            FactoryManager metalManager = new FactoryManager(new MetalDoorFactory());
+            Console.WriteLine("Metal Door");
+            metalManager.ShowDoor("195x85", "Black");
+
+
+        }
+    }
+}
